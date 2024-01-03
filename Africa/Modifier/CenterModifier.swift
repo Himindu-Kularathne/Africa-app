d9c1092410c38b038889800fbff95f5dac460273
Himindu <himindukularathne@gmail.com>
2024-01-03 10:17:33 +0530

    final commit , checked with ipad and macbook. further , app credits view is integrated

---
+++ Africa/Modifier/CenterModifier.swift
@@ -0,0 +1,18 @@
+//
+//  CenterModifier.swift
+//  Africa
+//
+//  Created by Himindu Kularathne on 2024-01-03.
+//
+
+import SwiftUI
+
+struct CenterModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CenterModifier()
+}
---
+++ Africa/View/CreditsView.swift
@@ -0,0 +1,18 @@
+//
+//  CreditsView.swift
+//  Africa
+//
+//  Created by Himindu Kularathne on 2024-01-03.
+//
+
+import SwiftUI
+
+struct CreditsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreditsView()
+}
